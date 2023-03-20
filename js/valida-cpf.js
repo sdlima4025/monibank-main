66ce0eadd62a5dbc323ceaf7ad5c3c28017b567e
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-20 19:06:18 -0300

    implementado function validaCPF digito verificaro 1 e 2

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,10 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, '');
-    validaNumerosRepetidos(cpf);
-
-    console.log(validaNumerosRepetidos(cpf));
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("CPF invalido!");
+    } else {
+        console.log("CPF valido!");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -32,5 +34,27 @@ function validaPrimeiroDigito(cpf) {
     }
 
     soma = (soma * 10) % 11;
+    if(soma == 10 || soma == 11) { // é 1 ou 11?
+        soma = 0;
+    }
 
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+    if(soma == 10 || soma == 11) { // é 1 ou 11?
+        soma = 0;
+    }
+
+
+    return soma != cpf[10];
 }
