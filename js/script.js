bbe1a12710aeb38e666d1e4643ebd02249b1684d
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-20 18:13:38 -0300

    criada function verifica campo selecionando via requires

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
+
+})
+
+function verificaCampo(campo) {
+
+}
